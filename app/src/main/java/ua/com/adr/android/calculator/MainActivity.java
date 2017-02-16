f1a33bf4d4400829e1187e7183a2623c5271dd0e
Andy <andreas04@yandex.ru>
2017-02-16 09:44:10 +0200

    Added bitmap design

--- app/src/main/java/ua/com/adr/android/calculator/MainActivity.java
+++ app/src/main/java/ua/com/adr/android/calculator/MainActivity.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.content.pm.ActivityInfo;
 
 public class MainActivity extends Activity {
 
@@ -37,7 +36,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         txtResult = (EditText) findViewById(R.id.txtResult);
 
